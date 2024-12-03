c4d78e24b9be2c11c6609dc095a8e16f94e266f5
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-12-03 17:14:00 +0100

    add article modify 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import articles from "./db/articles";
 
 function App() {
   const [articlesList, setArticleList] = useState(articles);
-  const [input, setInput] = useState("");
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputAuthor, setInputAuthor] = useState("");
 
   // click handler
   const handleInput = (e) => {
-    setInput(e.target.value);
+    setInputTitle(e.target.value);
+    setInputAuthor(e.target.value);
   };
 
   // submit handler
@@ -16,9 +18,23 @@ function App() {
     e.preventDefault();
 
     const newArticleList = [...articlesList];
-    newArticleList.push({ title: input });
+    newArticleList.push({ title: inputTitle, author: inputAuthor });
 
     setArticleList(newArticleList);
+  };
+
+  // edit item handler
+  const editItem = (modifyArticle, index) => {
+    const modifyArticleList = [...articlesList];
+
+    const newModifyArray = modifyArticleList.map((articleItem) =>
+      articleItem == modifyArticle
+        ? { title: "ciao", author: "ciao" }
+        : articleItem
+    );
+
+    console.log(newModifyArray);
+    setArticleList(newModifyArray);
   };
 
   // delete title handler
@@ -38,7 +54,9 @@ function App() {
         </div>
 
         <form onSubmit={articlesListHandler}>
-          <input type="text" onChange={handleInput} />
+          <input type="text" onChange={handleInput} placeholder="Titolo" />
+          <input type="text" onChange={handleInput} placeholder="Autore" />
+
           <button>Crea</button>
         </form>
 
@@ -48,10 +66,22 @@ function App() {
           <ul>
             {articlesList.map((article, index) => (
               <li key={index}>
-                <div className="title-container">
-                  <h2>{article.title}</h2>
-                  <button onClick={() => deleteItemHandler(article)}>
-                    <i className="fa-solid fa-trash"></i>
+                <div className="article-container">
+                  <div className="article-content">
+                    <h2>{article.title}</h2>
+                    <span>{article.author}</span>
+                  </div>
+                  <button
+                    onClick={() => editItem(article, index)}
+                    className="button"
+                  >
+                    <i className="fa-solid fa-pen-to-square edit"></i>
+                  </button>
+                  <button
+                    onClick={() => deleteItemHandler(article)}
+                    className="button"
+                  >
+                    <i className="fa-solid fa-trash trash"></i>
                   </button>
                 </div>
               </li>
--- src/db/articles.js
+++ src/db/articles.js
@@ -1,12 +1,15 @@
 const articles = [
   {
     title: "Title1",
+    author: "Author1",
   },
   {
     title: "Title2",
+    author: "Author2",
   },
   {
     title: "Title3",
+    author: "Author3",
   },
 ];
 
